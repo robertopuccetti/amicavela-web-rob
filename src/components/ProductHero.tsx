e82b458fe9aaa1dd98a88b8f3237bc512ab4af71
Ettore <puccio-94@live.it>
2023-06-11 01:29:18 +0200

    removed local pdf

--- src/components/ProductHero.tsx
+++ src/components/ProductHero.tsx
@@ -1,27 +1,33 @@
+import MuiTypography from "@mui/material/Typography";
+import Link from "../Link";
 import Button from "./Button";
 import ProductHeroLayout from "./ProductHeroLayout";
-import MuiTypography from "@mui/material/Typography";
-import Link  from "../Link";
 
 export default function ProductHero() {
-
   return (
     <ProductHeroLayout>
       <MuiTypography
         color="inherit"
         variant="h3"
         align="center"
-        sx={{ marginBottom: "0.5rem" }}>
+        sx={{ marginBottom: "0.5rem" }}
+      >
         AmicaVela
       </MuiTypography>
       <MuiTypography
         color="inherit"
         variant="h6"
         align="center"
-        sx={{ marginBottom: "10vh" }}>
+        sx={{ marginBottom: "10vh" }}
+      >
         associazione sportiva dilettantistica
       </MuiTypography>
-      <Link href="/pdf/tesseramento.pdf" target="_blank" underline="none" color="inherit">
+      <Link
+        href="https://pub-e5d8e266486a43f8be346cf1d6987202.r2.dev/tesseramento.pdf"
+        target="_blank"
+        underline="none"
+        color="inherit"
+      >
         <Button
           color="inherit"
           variant="outlined"
